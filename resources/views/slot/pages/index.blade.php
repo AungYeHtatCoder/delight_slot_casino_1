2c90aba1582a58e92c3d8e74ddff468dffb6cc37
Thein Thein Soe <theintheinsoe@xyberplanet.com>
2024-01-05 11:22:23 +0630

    ui fix

--- resources/views/slot/pages/index.blade.php
+++ resources/views/slot/pages/index.blade.php
@@ -4,11 +4,11 @@
 <!-- CAROUSEL START -->
 <div class="" style="margin-top: 60px; padding-top: 10px">
   <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade px-2" data-bs-ride="carousel">
-    <div class="carousel-indicators">
+    {{-- <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-    </div>
+    </div> --}}
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="{{ asset('slot_app/images/banner/banner1.png') }}" style="max-height: 500px" class="d-block w-100" alt="..." />
